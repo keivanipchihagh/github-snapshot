5a83b76203014850f4b4283f543109e175805dc3
Keivan Ipchi Hagh <keivanipchihagh@gmail.com>
2022-12-23 23:10:21 +0330

    feat: add local storage

--- js/scripts.js
+++ js/scripts.js
@@ -3,28 +3,82 @@ function show_preview() {
     username = $("#username-input").val();
     github_page = (username != "") ? ("github.com/" + username) : "";
     github_page_url = (username != "") ? ("https://" + github_page) : "";
-
     document.getElementById("link-preview").innerText = github_page
     document.getElementById("link-preview").setAttribute("href", github_page_url);
 }
 
-// Sends a request to the GitHub API to retrieve the user data
+// Updates the interface from either LocalStorage or the GitHub API
 function submit_username() {
-    var xhttp = new XMLHttpRequest();
     username = document.getElementById("username-input").value
-    url = "https://api.github.com/users/" + username;
+    var data;
+    if (username_exists_in_local(username)) {
+        data = read_from_local(username)
+        console.log("Retrieved " + username + "'s data from LocalStorage.")
+    } else {
+        data = retrieve_from_github(username);
+        console.log("Retrieved " + username + "'s data from GitHub.")
+        save_to_local(data);
+    }
+    update_interface(data);
+}
 
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            var data = JSON.parse(this.responseText);
-            document.getElementById("fullname").innerText = data["name"];
-            document.getElementById("username").innerText = "@" + data["login"];
-            document.getElementById("avatar").setAttribute("src", data["avatar_url"]);
-            document.getElementById("bio").innerText = data["bio"];
-            document.getElementById("blog").innerText = data["blog"];
-            document.getElementById("location").innerText = data["location"];
-        }
-    };
-    xhttp.open("GET", url, true);
-    xhttp.send();
+// Retrieve the user data from the GitHub API
+function retrieve_from_github(username) {
+    response = httpRequest("https://api.github.com/users/" + username, "GET")
+    if (response.status == 200) {
+        data = JSON.parse(response.responseText)
+        console.log(data);
+        return data
+    }
+    console.log("Error: " + response.status + " " + response.statusText);
+}
+
+// Sends an HTTP request to the given address and waits for a response
+function httpRequest(address, reqType, asyncProc) {
+    var req = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+    if (asyncProc) { 
+      req.onreadystatechange = function() { 
+        if (this.readyState == 4) {
+          asyncProc(this);
+        } 
+      };
+    }
+    req.open(reqType, address, !(!asyncProc));
+    req.send();
+    console.log("Sent " + reqType + " request to " + address + " (Status" + req.status + ")")
+    return req;
+}
+
+// Update the page objects with the given user data
+function update_interface(data) {
+    document.getElementById("fullname").innerText = data["name"];
+    document.getElementById("username").innerText = "@" + data["login"];
+    document.getElementById("avatar").setAttribute("src", data["avatar_url"]);
+    document.getElementById("bio").innerText = data["bio"];
+    document.getElementById("blog").innerText = data["blog"];
+    document.getElementById("location").innerText = data["location"];
+    console.log("Updated the page objects with " + data["login"] + "'s data.")
+}
+
+// Stores the given user data to LocalStorage
+function save_to_local(data) {
+    localStorage.setItem(data["login"], JSON.stringify(data));
+    console.log("Saved " + data["login"] + "'s data to LocalStorage.")
+}
+
+// Checks if the given username exists in LocalStorage
+function username_exists_in_local(username) {
+    return localStorage.getItem(username) != null
+}
+
+// Reads the user data from LocalStorage
+function read_from_local(username) {
+    return JSON.parse(localStorage.getItem(username))
+}
+
+// Shows an error message on the alert box
+function show_err(message) {
+    document.getElementById("alert").innerText = message;
+    document.getElementById("alert").style.opacity = '1';
+    setTimeout(function(){document.getElementById("alert").style.opacity = '0'}, 2000);
 }
