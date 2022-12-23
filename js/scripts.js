16e5c0f4260cf28c4663b1f7a5b1165844bd240f
Keivan Ipchi Hagh <keivanipchihagh@gmail.com>
2022-12-23 21:09:51 +0330

    feat: minor styles improvements + add AJAX request (CORS covered)

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,41 @@
 $(document).ready(function() {
+
+    // Show a preview of the actual GitHub link and a hyperlink to it
     $("body").on("input", "#username-input", function() {
-        input_value = $("#username-input").val();
-        if (input_value != "") {
-            $("#link-preview").attr("href", "https://github.com/" + $("#username-input").val());
-            $("#link-preview").text("github.com/" + $("#username-input").val());
-        } else {
-            $("#link-preview").attr("href", "");
-            $("#link-preview").text("");
-        }
+
+        username = $("#username-input").val();
+        github_page = (username != "") ? ("github.com/" + username) : "";
+        github_page_url = (username != "") ? ("https://" + github_page) : "";
+
+        $("#link-preview").text(github_page);
+        $("#link-preview").attr("href", github_page_url);
+    });
+
+    // Submit the form and retrieve the GitHub user data
+    $("#submit").click(function() {
+        $.ajax({
+            url: "https://api.github.com/users/" + $("#username-input").val(),
+            type: "GET",
+            dataType: "jsonp",  // JSONP is required to avoid CORS
+            headers: {
+                "accept": "application/json",
+                "Access-Control-Allow-Origin": "x-requested-with",  // CORS header
+            },
+            success: function(response) {
+                data = response["data"]
+                avatar_url = data["avatar_url"]
+                bio = data["bio"]
+                blog = data["blog"]
+                company = data["company"]
+                followers = data["followers"]
+                followings = data["following"]
+                location = data["location"]
+                name = data["name"]
+                public_repos = data["public_repos"]
+            },
+            error: function(response) {
+                alert(response);
+            }
+        });
     });
 })
